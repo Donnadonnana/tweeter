687286a93b56ebea8ef4c5d1c0a4c758e2c24733
donnahuang <donnaphzj@gmail.com>
2022-05-22 18:01:37 -0400

    add tweeter secutrity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,12 +29,19 @@
 //       "created_at": 1461113959088
 //     }
 //   ]
+const escapeHTML = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const createTweetElement = (data) => {
   var date = timeago.format(data.created_at);
+  const content = escapeHTML(data.content.text);
   const $tweet = $(`
   <section class="existing-tweet"><article class="existing-tweet-article">
-          <header> 🧒 ${data.user.name} </header>
-          <p>${data.content.text}</p>
+          <header> 🧒 ${escapeHTML(data.user.name)} </header>
+          <p>${content}</p>
           <footer class="existing-tweet-footer">${date}
             <div>
               <i class="fa-solid fa-flag"></i>
@@ -47,7 +54,7 @@ const createTweetElement = (data) => {
 }
 
 const renderTweets = (data) => {
-  
+  $('#tweets-container').empty();
   for (let eachUser of data) {
     const $tweet = createTweetElement(eachUser);
     $('#tweets-container').append($tweet);
@@ -56,6 +63,15 @@ const renderTweets = (data) => {
 }
 
 // renderTweets(data);
+
+const loadtweets = () => {
+  $.ajax({
+    url: 'http://localhost:8080/tweets',
+    method: 'GET',
+  }).then((res) => {
+    renderTweets(res);
+  })
+}
 
 $('#submit-button').submit(function (event) {
   const $tweetText = $('#tweet-text').serialize();
@@ -72,17 +88,11 @@ $('#submit-button').submit(function (event) {
     method: 'POST',
     data:$tweetText
   }).then((res)=> {
-    console.log(res);
+    loadtweets();
+    $('#tweet-text').val("");
 })
   }  
 })
 
-const loadtweets = () => {
-  $.ajax({
-    url: 'http://localhost:8080/tweets',
-    method: 'GET',
-  }).then((res)=> {
-    renderTweets(res);
-})
-}
+
 loadtweets();
